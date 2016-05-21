b321c4482d7acf3624f08c5a5b5b26662d492d2d
Stevey Z <sjzerneri@gmail.com>
2016-05-21 10:18:19 -0400

    adding specs

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var app = angular.module('myApp', []);
-
-app.directive('optIn', function () {
-    return {
-        templateUrl: 'opt-in-template.html',
-        restrict: 'AE',
-        transclude: true,
-    }
-});
+angular.module('myApp', [])
+    .directive('optIn', function () {
+        return {
+            templateUrl: 'opt-in-template.html',
+            restrict: 'AE',
+            transclude: true,
+        }
+    });
---
+++ appSpec.js
@@ -0,0 +1,20 @@
+//it should have an image that shows before the form
+
+//it should have a last name input box
+
+describe('optIn', function () {
+
+    var $compile,
+        $rootScope;
+    beforeEach(module("myApp"));
+    beforeEach(module('opt-in-template.html'));
+    beforeEach(inject(function (_$rootscope_, _$compile_) {
+        $compile = _$compile_;
+        $rootScope = _$rootscope_;
+    }));
+    it('should have a submit button that says Give us yr infoz!', function () {
+        var element = $compile("<opt-in></opt-in>")($rootScope);
+        $rootScope.digest();
+        expect(element.html()).toContain("Give us yr infoz!");
+    })
+});
